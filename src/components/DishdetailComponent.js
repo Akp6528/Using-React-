452dd99e55c633b3bebebc8351fad8af6c4c5c21
Ashwani Pandey <pandeyashwani999@gmail.com>
2020-05-31 01:58:42 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,7 +4,7 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";  
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 function RenderDish({dish}) {
 
     return (
@@ -169,9 +169,27 @@ class CommentForm extends Component {
 
 
 const Dishdetail = (props) => {
-
-  if (props.dish != null)
-      return (
+  
+  if (props.isLoading) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <Loading />
+            </div>
+        </div>
+    );
+}
+else if (props.errMess) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <h4>{props.errMess}</h4>
+            </div>
+        </div>
+    );
+}
+else if (props.dish != null) 
+    return (
           <div className="container">
               <div className="row">
                   <Breadcrumb>
